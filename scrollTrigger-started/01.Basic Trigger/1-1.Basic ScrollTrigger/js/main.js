b23fe6a275f47cd065cf72b1fb1d7eb2fa811956
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-05-06 13:54:22 +0900

    📝 basic trigger

--- scrollTrigger-started/01.Basic Trigger/1-1.Basic ScrollTrigger/js/main.js
+++ scrollTrigger-started/01.Basic Trigger/1-1.Basic ScrollTrigger/js/main.js
@@ -1,7 +1,40 @@
+// 🖐️ scrollTrigger 쓰는 방법
 
+// - tween 안에 연결
+// gsap.to(".tiger", {
+//   x: 500,
+//   rotation: 360,
+//   duration: 3,
+//   scrollTrigger: {
+//     trigger: ".tigerSection",
+//     markers: true,
+//     start: "top center",
+//     end: "bottom center",
+//     id: "tiger",
+//   },
+// });
 
+// - 생성자로 만들어서
+const tween = gsap.to(".tiger", {
+  x: 500,
+  rotation: 360,
+  duration: 3,
+});
 
+ScrollTrigger.create({
+  trigger: ".tigerSection",
+  markers: true,
+  start: "top center",
+  end: "bottom center",
+  id: "tiger",
+  animation: tween,
+  toggleActions: "restart pause reverse pause",
+});
 
+// 🪄 toggleActions
+// 연결된 애니메이션이 4개의 고유한 토글 위치에 따라 순서대로 제어되는 방식을 결정
+// 기본값은 play none none none
 
-
-
+// events: onEnter onLeave onEnterBack onLeaveBack
+// toggleActions: "restart resume reverse reset";
+//options: play, pause, resume, reset, restart, complete, reverse, none
