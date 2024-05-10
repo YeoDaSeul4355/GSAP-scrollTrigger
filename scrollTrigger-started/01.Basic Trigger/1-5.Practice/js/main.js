7bdef8022cd82caf6f2954ae564f0c567cdbf8f7
YeoDaSeul4355 <yds43557340@gmail.com>
2024-05-10 14:26:38 +0900

    ⚡️ basic-scrollTrigger practice

--- scrollTrigger-started/01.Basic Trigger/1-5.Practice/js/main.js
+++ scrollTrigger-started/01.Basic Trigger/1-5.Practice/js/main.js
@@ -25,20 +25,61 @@
 //   scrub: true,
 // });
 
-// 반복문을 통해 지그재그 교차 애니메이션 (스크롤 트리거)
-gsap.utils.toArray("section").forEach((section, index) => {
-  const w = section.querySelector(".wrapper");
+const showDemo = () => {
+  // 반복문을 통해 지그재그 교차 애니메이션 (스크롤 트리거)
+  gsap.utils.toArray("section").forEach((section, index) => {
+    const w = section.querySelector(".wrapper");
 
-  if (w) {
-    const [x, xEnd] =
-      index % 2
-        ? ["100%", -(w.scrollWidth - innerWidth)]
-        : [-(w.scrollWidth - innerWidth), 0];
+    if (w) {
+      const [x, xEnd] =
+        index % 2
+          ? ["100%", -(w.scrollWidth - innerWidth)]
+          : [-(w.scrollWidth - innerWidth), 0];
 
-    gsap.fromTo(
-      w,
-      { x },
-      { x: xEnd, scrollTrigger: { trigger: section, scrub: 0.5 } }
-    );
-  }
-});
+      gsap.fromTo(
+        w,
+        { x },
+        { x: xEnd, scrollTrigger: { trigger: section, scrub: 0.5 } }
+      );
+    }
+  });
+};
+
+const awesome = () => {
+  const tl = gsap
+    .timeline({
+      defaults: {
+        ease: "none",
+      },
+    })
+    .from(".awesome .text", { x: innerWidth })
+    .to(".awesome .text", { scale: 50, xPercent: -100 })
+    .to("body", { duration: 0.3, backgroundColor: "black" }, "-=0.5");
+
+  ScrollTrigger.create({
+    trigger: ".awesome",
+    start: "top top",
+    end: "+=3000",
+    pin: true,
+    animation: tl,
+    scrub: 1,
+  });
+};
+
+const tryNow = () => {
+  ScrollTrigger.create({
+    trigger: ".try",
+    start: "top top",
+    end: "+=2000",
+    animation: gsap.from(".try .text", { y: 50, opacity: 0 }),
+    pin: true,
+    scrub: true,
+  });
+};
+
+function init() {
+  showDemo();
+  awesome();
+  tryNow();
+}
+init();
