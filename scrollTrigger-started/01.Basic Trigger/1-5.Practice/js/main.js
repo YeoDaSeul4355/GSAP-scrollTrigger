70ced44fee31b4e31340b3e17569c06abd689132
YeoDaSeul4355 <yds43557340@gmail.com>
2024-05-10 14:51:55 +0900

    ⚡️ 로딩 기능 추가, ♻️main.js 리팩토링

--- scrollTrigger-started/01.Basic Trigger/1-5.Practice/js/main.js
+++ scrollTrigger-started/01.Basic Trigger/1-5.Practice/js/main.js
@@ -1,32 +1,10 @@
-// const tween = gsap.fromTo(
-//   ".wrapper.text",
-//   { x: "100%" },
-//   {
-//     x() {
-//       return -(this.targets()[0].scrollWidth - innerWidth);
-//     },
-//   }
-// );
-
-// const tween = gsap.fromTo(
-//   ".wrapper.text",
-//   {
-//     x() {
-//       return -this.targets()[0].scrollWidth;
-//     },
-//   },
-//   { x: 0 }
-// );
-
-// ScrollTrigger.create({
-//   trigger: ".demo-text",
-//   animation: tween,
-//   markers: true,
-//   scrub: true,
-// });
-
 const showDemo = () => {
   // 반복문을 통해 지그재그 교차 애니메이션 (스크롤 트리거)
+  gsap.to(".loader", { autoAlpha: 0 });
+  document.querySelector("body").style.overflow = "auto";
+
+  // 새로고침 할 때 위로 가는 기능
+  document.scrollingElement.scrollTo(0, 0);
   gsap.utils.toArray("section").forEach((section, index) => {
     const w = section.querySelector(".wrapper");
 
@@ -82,4 +60,25 @@ function init() {
   awesome();
   tryNow();
 }
-init();
+
+// 😁 로딩 함수 구현
+// 유사 배열을 배열로 만들어서 쓰는게 좋음
+// 보통은 gsap.utils.toArray()로 배열을 만든다.
+const img = gsap.utils.toArray("img");
+const loader = document.querySelector(".loader--text");
+
+// gsap를 쓰지 않을 시 유사배열을 배열로 만들어주는 방법 3가지
+// const img = Array.from(document.querySelectorAll("img"));
+// const img = Array.prototype.slice.call(document.querySelectorAll("img"));
+// const img = [...document.querySelectorAll("img")]
+
+const updateProgress = (instance) => {
+  loader.textContent = `${Math.round(
+    (instance.progressedCount * 100) / img.length
+  )}%`;
+};
+
+imagesLoaded(img)
+  .on("progress", updateProgress)
+  // 로딩 후 처리
+  .on("always", init);
