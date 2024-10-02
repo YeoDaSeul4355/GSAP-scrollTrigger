ae0fb49c854ed8808f023d0cf5e4b76cdebacec3
YeoDaSeul4355 <yds43557340@gmail.com>
2024-10-02 17:18:55 +0900

    📝 Prevent Scroll 작성

---
+++ scrollTrigger-started/02.Advanced Trigger/2-3.PreventScroll/README.md
@@ -0,0 +1,30 @@
+# 🌠 Prevent Scroll
+
+## 🙌 Prevent Scroll
+
+Intro페이지나 Hero페이지에서 애니메이션을 보여주고 스크롤 활성화하는 방법을 알아보자.<br />
+스크롤을 막지 않으면 애니메이션이 끝나기도 전에 스크롤을 내리는 경우가 많다. 
+
+### 스크롤을 잠그거나 방지하는 방법
+
+- css로 ```body``` 요소에 ```overflow: hidden```를 적용하면 스크롤이 비활성화 된다.
+```css
+body {
+  overflow: hidden;
+}
+```
+
+- ```eventCallback```활용하기
+```javascript
+// 애니메이션 타임라인 생성
+const tl = // 애니메이션 생성
+
+// 애니메이션이 끝나고 함수 불러옴
+tl.eventCallback('onComplete', () => {
+  document.body.style.overflow = ''; 
+})
+```
+
+### 📎 실습 파일 살펴보기
+- [index.html](<https://github.com/YeoDaSeul4355/GSAP-scrollTrigger/blob/main/scrollTrigger-started/02.Advanced%20Trigger/2-2.Callback/index.html>)
+- [js/main.js](<https://github.com/YeoDaSeul4355/GSAP-scrollTrigger/blob/main/scrollTrigger-started/02.Advanced%20Trigger/2-2.Callback/js/main.js>)
--- scrollTrigger-started/02.Advanced Trigger/2-3.PreventScroll/js/main.js
+++ scrollTrigger-started/02.Advanced Trigger/2-3.PreventScroll/js/main.js
@@ -1,25 +1,48 @@
+const tl = gsap.timeline({
+  defaults: {
+    duration: 3,
+    ease: 'power3.inOut',
+  },
+  // onComplete: () => {
+  //   console.log('끝!');
+  // }
+})
+
+.from('.image_container > div', {
+  x: innerWidth,
+  stagger: {
+    amount: 0.5
+  },
+})
+.to('.image_container > div', {
+  y: innerHeight *  0.2,
+  stagger: {
+    amount: 0.5,
+    from: 2
+  },
+}, '-=1.5')
+.from('.text_container .elem', {
+  y: 50,
+  opacity: 0,
+  stagger: {
+    each: 0.2,
+    ease: 'power3.inOut'
+  },
+  duration: 1
+}, '-=2')
+
+// 애니메이션이 끝나고 스크롤 풀기  
+tl.eventCallback('onComplete', () => {
+  gsap.set('#no-scroll', {display: 'none'});
+})
+
+ScrollTrigger.create({
+  trigger: '.section01',
+  start: 'top top',
+  end: 'bottom top',
+  animation: gsap.to('.image_container', {x: -innerWidth * 0.5}),
+  scrub: true
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-markers()
-
-
-
+markers();
